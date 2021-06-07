c3808e67f433092046b8044f52b3ea3c641c356d
Diksha katna <84858977+Diksha3012@users.noreply.github.com>
2021-06-07 08:53:14 +0530

    Update 10thtestcase.spec.js

--- radio-button-using-css-selector/10thtestcase.spec.js
+++ radio-button-using-css-selector/10thtestcase.spec.js
@@ -8,12 +8,9 @@ describe('handling GUI elements',function ()
      
     cy.root()
     cy.visit("http://demo.guru99.com/test/newtours/")
-    //cy.url().should('include','newtours')
-    //cy.get("[type='text']").should('be.visible').should('be.enabled').type('dikshkatna912@gmail.com')
-    //cy.get("[type='password']").type('diksha@12')
-    //cy.get("[type='submit']").click()
+    
     cy.get(':nth-child(2) > [width="80"] > a').click()
-    //cy.get(".container-fluid>a").click()
+   
     cy.get("[value='roundtrip']").should('be.visible').should('be.checked')
     //cy.get("[value='oneway']").should('be.visible').should('not.be.checked')
     cy.get("[value='oneway']").should('be.visible').should('not.be.checked').click()
